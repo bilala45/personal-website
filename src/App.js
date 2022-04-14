219b38a2770e33e55c937c42ccbedd2e420fea28
Bilal Ali <bilalali2@hotmail.com>
2022-04-14 18:34:59 -0400

    add header and about text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 const App = () => {
-  return <div>Hello, world</div>;
+  return (
+    <div>
+      <h1>Bilal Ali</h1>
+      <h2>CS @ UPenn | links</h2>
+
+      <p>Hi, I’m Bilal!</p>
+      <p>
+        I’m a Master’s student studying computer science at the University of
+        Pennsylvania. In 2021, I graduated from McGill University, where I
+        studied physiology and math and competed for McGill’s track and field
+        team. During my undergraduate studies, I was fortunate enough to partake
+        in coursework and research that helped me re-discover my interests
+        through the lens of computing. My transition to computer science was
+        inspired by these transformative experiences.
+      </p>
+      <p>
+        Things I particularly like: wearable technology, Chris Paul, corgis, and
+        good music.
+      </p>
+    </div>
+  );
 };
 
 export default App;
