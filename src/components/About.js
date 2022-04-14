41a630b65486ebde901cb945ad691250d66729a0
Bilal Ali <bilalali2@hotmail.com>
2022-04-14 18:40:57 -0400

    add components for about and site header

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div>
+      <p>Hi, I’m Bilal!</p>
+      <p>
+        I’m a Master’s student studying computer science at the University of
+        Pennsylvania. In 2021, I graduated from McGill University, where I
+        studied physiology and math and competed for McGill’s track and field
+        team. During my undergraduate studies, I was fortunate enough to partake
+        in coursework and research that helped me re-discover my interests
+        through the lens of computing. My transition to computer science was
+        inspired by these transformative experiences.
+      </p>
+      <p>
+        Things I particularly like: wearable technology, Chris Paul, corgis, and
+        good music.
+      </p>
+    </div>
+  );
+};
+
+export default About;
