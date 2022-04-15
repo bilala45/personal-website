ac80c8ea8813cf9ccbb02b615d79c0f3e69ab8e2
Bilal Ali <bilalali2@hotmail.com>
2022-04-15 02:09:19 -0400

    update contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,16 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
+  const github = "https://github.com/bilala45";
+  const linkedin = "https://www.linkedin.com/in/bilal-ali1/";
+  const email = "mailto: bilalali2@hotmail.com";
+
   return (
     // add padding above and below links and center
     <div className="py-12 flex justify-center">
-      <Link icon={faGithub} />
-      <Link icon={faLinkedin} />
-      <Link icon={faEnvelope} />
+      <Link icon={faGithub} link={github} />
+      <Link icon={faLinkedin} link={linkedin} />
+      <Link icon={faEnvelope} link={email} />
     </div>
   );
 };
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // accepts icon name as prop
-const Link = ({ icon }) => {
+const Link = ({ icon, link }) => {
   return (
     <span className="text-5xl px-5">
-      <FontAwesomeIcon icon={icon} />
+      <a href={link}>
+        <FontAwesomeIcon icon={icon} />
+      </a>
     </span>
   );
 };
