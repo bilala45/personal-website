c5f8c7e3c09da316297243fba6b193fd2578c038
Bilal Ali <bilalali2@hotmail.com>
2022-04-15 01:57:14 -0400

    add footer for copyright and source code link

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    // elements placed in flex container to place at both ends
+    <div className="flex justify-between py-2">
+      <span>Copyright &copy; 2022 Bilal Ali</span>
+      <a
+        href="https://github.com/bilala45/personal-website"
+        className="hover:underline decoration-4 underline-offset-2 decoration-[#00bad6]"
+      >
+        source code <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
