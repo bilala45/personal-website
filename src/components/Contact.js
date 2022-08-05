dd9a38ba82e1f0769c30106cd7d0a67f12b50bf5
Bilal Ali <bilalal495@outlook.com>
2022-08-05 03:22:17 -0400

    Update individual links in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,18 @@
-import Link from "./Link";
+import ContactLink from "./ContactLink";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   const github = "https://github.com/bilala45";
   const linkedin = "https://www.linkedin.com/in/bilal-ali1/";
-  const email = "mailto: bilalal495@outlook.com";
+  const email = "mailto: bilalali@seas.upenn.edu";
 
   return (
     // add padding above and below links and center
-    <div className="my-12 py-2 px-2 rounded-full max-w-fit bg-gradient-to-r from-sky-500 to-indigo-500">
-      <Link icon={faGithub} link={github} />
-      <Link icon={faLinkedinIn} link={linkedin} />
-      <Link icon={faEnvelope} link={email} />
+    <div className="my-12 py-1 px-1.5 rounded-full max-w-fit bg-gradient-to-r from-sky-500 to-indigo-500">
+      <ContactLink icon={faGithub} link={github} />
+      <ContactLink icon={faLinkedinIn} link={linkedin} />
+      <ContactLink icon={faEnvelope} link={email} />
     </div>
   );
 };
