99e8dd2f3ef381184d1412873eee9ef25db5115d
Bilal Ali <bilalal495@outlook.com>
2022-08-05 19:41:37 -0400

    Set up routing and paths using React Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import ScrollProgress from "./components/ScrollProgress";
 import Content from "./components/Content";
 import Intro from "./components/Intro";
 
-const App = () => (
+const App = () => {
   // comment out footer for now and add it back in at the end
   // Wrap content and footer in a div to restrict the max screen size
-  <body className="w-screen min-h-screen bg-[#edf6f9]">
-    <ScrollProgress />
-    <div className="max-w-screen-lg mx-auto pt-32">
-      <Intro />
-      <Content />
+  return (
+    <div className="w-screen min-h-screen bg-[#edf6f9]">
+      <ScrollProgress />
+      <div className="max-w-screen-md mx-auto pt-32">
+        <Intro />
+        <Content />
+      </div>
     </div>
-  </body>
-);
+  );
+};
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
 import About from "./About";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const Main = () => (
   <div className="pr-4">
     {/* intro and about occupy height of screen at minimum (if content is longer than screen height, then it'll expand past it) */}
-    <About />
+    <Routes>
+      <Route path="/" element={<About />} />
+      <Route path="/about" element={<About />} />
+      {/* <Route path="/projects" element={<Resources />} />
+      <Route path="/resources" element={<Resources />} /> */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   </div>
 );
 
