551db9b719166ace958aebc2b09f560764d271e9
Bilal Ali <bilalal495@outlook.com>
2022-08-05 03:17:05 -0400

    Add component for individual routes in nav bar

---
+++ src/components/NavLink.js
@@ -0,0 +1,7 @@
+const NavLink = ({ title }) => (
+  <div className="mt-4 font-medium text-lg w-fit hover:underline hover:text-indigo-500 underline-offset-2 decoration-4 cursor-pointer">
+    {title}
+  </div>
+);
+
+export default NavLink;
