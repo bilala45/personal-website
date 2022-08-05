cf7807b36310a515a0c342d4c3323ce50b397ddf
Bilal Ali <bilalal495@outlook.com>
2022-08-05 03:19:46 -0400

    Add component for individual links in contact section

---
+++ src/components/ContactLink.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// accepts icon name as prop
+const ContactLink = ({ icon, link }) => {
+  // add breakpoint to remove white highlight for mobile
+  return (
+    <span className="text-3xl px-4 text-white rounded-full hover:text-black hover:bg-white">
+      <a href={link}>
+        <FontAwesomeIcon icon={icon} />
+      </a>
+    </span>
+  );
+};
+
+export default ContactLink;
