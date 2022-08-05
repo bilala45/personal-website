aeb81383a2dd8ff95fa27fe5545d91e7f7f5cb69
Bilal Ali <bilalal495@outlook.com>
2022-08-05 03:19:03 -0400

    Update text in About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,35 @@
 const About = () => {
   return (
-    <div className="pt-6">
-      I'm currently a Master's student at the{" "}
-      <a
-        className="text-indigo-500 font-semibold hover:underline underline-offset-2 decoration-4"
-        href="https://www.upenn.edu/"
-      >
-        University of Pennsylvania
-      </a>
-      , studying computer science.
-      <div className="pt-4">
-        Previously, I studied physiology and mathematics at{" "}
+    <div className="text-lg">
+      <div className="text-2xl">Hey, I'm Bilal! &#128075;&#127997;</div>
+      <div className="pt-3">
+        I'm currently a Master's student at the{" "}
         <a
-          className="text-indigo-500 font-semibold hover:underline underline-offset-2 decoration-4"
+          className="text-indigo-500 font-medium hover:underline underline-offset-2 decoration-4 hover:ease-in"
+          href="https://www.upenn.edu/"
+        >
+          University of Pennsylvania
+        </a>
+        , studying computer science.
+      </div>
+      <div className="pt-3">
+        In 2021, I graduated from{" "}
+        <a
+          className="text-indigo-500 font-medium hover:underline underline-offset-2 decoration-4"
           href="https://www.mcgill.ca/"
         >
-          McGill University
+          McGill Universit
         </a>
-        . My academic experiences there inspired me to explore how technology
-        can be used to help people lead healthier lives.
+        , where I studied physiology and mathematics.
+      </div>
+      <div className="pt-3">
+        In a previous life, I was a high jumper for McGill's track and field
+        team. Although I no longer compete, I've maintained my passion for
+        sports through weightlifting and biking.
+      </div>
+      <div className="pt-3">
+        <span className="font-semibold">Things I love:</span> wearable tech, my
+        (future) corgi, and Kendrick Lamar
       </div>
     </div>
   );
