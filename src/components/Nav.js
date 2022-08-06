f7d4433a42ae0c75ea2b5e59f1c33baa90ebb01e
Bilal Ali <bilalal495@outlook.com>
2022-08-05 20:54:01 -0400

    Update nav bar styling to display what page the user is on

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,13 @@
 import NavLink from "./NavLink";
 
-const Nav = () => (
-  <div className="pl-4">
-    <NavLink title="About" />
-    <NavLink title="Projects" />
-    <NavLink title="Resources" />
-  </div>
-);
+const Nav = () => {
+  return (
+    <div className="pl-4">
+      <NavLink title="About" />
+      <NavLink title="Projects" />
+      <NavLink title="Resources" />
+    </div>
+  );
+};
 
 export default Nav;
--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,14 +3,22 @@ import { useNavigate } from "react-router-dom";
 const NavLink = ({ title }) => {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(`/${title.toLowerCase()}`);
+  };
+
   return (
-    <div
-      onClick={() => {
-        navigate(`/${title.toLowerCase()}`);
-      }}
-      className="mt-4 font-medium text-lg w-fit hover:underline hover:text-indigo-500 underline-offset-2 decoration-4 cursor-pointer"
-    >
-      {title}
+    <div className="mt-4 font-medium text-lg w-fit hover:text-indigo-500 hover:underline underline-offset-2 decoration-4 cursor-pointer">
+      <div
+        onClick={handleClick}
+        className={
+          window.location.pathname == `/${title.toLowerCase()}`
+            ? "text-indigo-500"
+            : ""
+        }
+      >
+        {title}
+      </div>
     </div>
   );
 };
