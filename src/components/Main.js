70eac88e449edea19babc0ea7cb292550d4c58c9
Bilal Ali <bilalal495@outlook.com>
2022-08-05 20:52:51 -0400

    Fix routing to link to about page by default

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,15 @@
 import About from "./About";
+import Resources from "./Resources";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 const Main = () => (
   <div className="pr-4">
     {/* intro and about occupy height of screen at minimum (if content is longer than screen height, then it'll expand past it) */}
     <Routes>
-      <Route path="/" element={<About />} />
       <Route path="/about" element={<About />} />
-      {/* <Route path="/projects" element={<Resources />} />
-      <Route path="/resources" element={<Resources />} /> */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      {/* <Route path="/projects" element={<Projects />} /> */}
+      <Route path="/resources" element={<Resources />} />
+      <Route path="*" element={<Navigate to="/about" replace />} />
     </Routes>
   </div>
 );
