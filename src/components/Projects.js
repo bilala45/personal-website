8f3d96af00c1fc3ff2b69b863c96a859c067f0aa
Bilal Ali <bilalal495@outlook.com>
2022-08-07 03:00:03 -0400

    Add icons for project technologies

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,40 +1,61 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faDisplay } from "@fortawesome/free-solid-svg-icons";
-import { faReact, faJava, faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import {
+  faReact,
+  faJava,
+  faNodeJs,
+  faSpotify,
+  faNode,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => (
   <div className="text-md">
-    {/* Start of project one */}
-    <div className="text-lg font-semibold">
-      Git-lite{" "}
-      <span className="px-1.5 py-0.5 mr-1 ml-2 rounded-md bg-green-300 text-sm font-normal">
-        <FontAwesomeIcon className="mr-0.5" icon={faJava} /> Java
-      </span>
-    </div>
-    <div className="pt-3">
-      A command-line app that mimics the local features of Git, including
-      committing, merging, amending and branching.
-    </div>
-    <div className="pt-3">
-      <FontAwesomeIcon icon={faCode} />
-      <a href="https://github.com/bilala45/git-lite">
-        <span className="ml-2 font-medium text-indigo-500 hover:underline decoration-4 underline-offset-2">
-          Code
+    {/* Start of project */}
+    <div className="">
+      <div className="text-lg font-semibold">
+        Git-lite{" "}
+        <span className="px-1.5 py-0.5 mr-1 ml-2 rounded-md bg-sky-600 text-white text-sm font-normal">
+          <FontAwesomeIcon className="mr-0.5" icon={faJava} /> Java
         </span>
-      </a>
+        <span className="px-1.5 py-0.5 mx-1 rounded-md bg-sky-600 text-white text-sm font-normal">
+          <FontAwesomeIcon className="mr-1" icon={faGitAlt} />
+          Git
+        </span>
+      </div>
+      <div className="pt-3">
+        A command-line app that mimics the local features of Git, including
+        committing, merging, amending and branching.
+      </div>
+      <div className="pt-3">
+        <FontAwesomeIcon icon={faCode} />
+        <a href="https://github.com/bilala45/git-lite">
+          <span className="ml-2 font-medium text-indigo-500 hover:underline decoration-4 underline-offset-2">
+            Code
+          </span>
+        </a>
+      </div>
     </div>
 
-    {/* Start of project two */}
+    {/* Start of project */}
     <div className="pt-8">
       <div className="text-lg font-semibold">
         Amplify{" "}
-        <span className="px-1.5 py-0.5 mr-1 ml-2 rounded-md bg-red-300 text-sm font-normal">
+        <span className="px-1.5 py-0.5 mr-1 ml-2 rounded-md bg-sky-600 text-white text-sm font-normal">
           <FontAwesomeIcon className="mr-1" icon={faReact} />
           React
         </span>
-        <span className="px-1.5 py-0.5 mx-1 rounded-md bg-blue-300 text-sm font-normal">
-          <FontAwesomeIcon className="mr-1.5" icon={faNodeJs} />
+        <span className="px-1.5 py-0.5 mx-1 rounded-md bg-sky-600 text-white text-sm font-normal">
+          <FontAwesomeIcon className="mr-1.5" icon={faNode} />
           Node.js
+        </span>
+        <span className="px-1.5 py-0.5 mx-1 rounded-md bg-sky-600 text-white text-sm font-normal">
+          <FontAwesomeIcon className="mr-1.5" icon={faNodeJs} />
+          Express.js
+        </span>
+        <span className="px-1.5 py-0.5 mx-1 rounded-md bg-sky-600 text-white text-sm font-normal">
+          <FontAwesomeIcon className="mr-1.5" icon={faSpotify} />
+          Spotify API
         </span>
       </div>
       <div className="pt-3">
@@ -50,7 +71,6 @@ const Projects = () => (
             </span>
           </a>
         </span>
-
         <span>
           <FontAwesomeIcon icon={faDisplay} />
           <a href="https://github.com/bilala45/git-lite">
