8d4165f7bf3fa1156fa19e9775bf4dbf8359a2e9
Bilal Ali <bilalal495@outlook.com>
2022-08-07 01:57:54 -0400

    Add Footer component to webpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ScrollProgress from "./components/ScrollProgress";
 import Content from "./components/Content";
 import Intro from "./components/Intro";
+import Footer from "./components/Footer";
 
 const App = () => {
   // comment out footer for now and add it back in at the end
@@ -8,9 +9,12 @@ const App = () => {
   return (
     <div className="w-screen min-h-screen bg-[#edf6f9]">
       <ScrollProgress />
-      <div className="max-w-screen-md mx-auto pt-32">
-        <Intro />
-        <Content />
+      <div className="max-w-screen-md mx-auto">
+        <div className="py-28">
+          <Intro />
+          <Content />
+        </div>
+        <Footer />
       </div>
     </div>
   );
