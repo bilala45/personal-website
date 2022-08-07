dcc3249e0837d4118f1fd352ec427f77d345e74e
Bilal Ali <bilalal495@outlook.com>
2022-08-07 01:58:09 -0400

    Add component for resources

---
+++ src/components/Resources.js
@@ -0,0 +1,79 @@
+const Resources = () => {
+  return (
+    <div className="text-md">
+      <div className="text-lg">
+        Much of what I've learned about computer science and math has come from
+        various free online resources. Here are some of the ones I've enjoyed
+        most.
+      </div>
+      <div>
+        <div className="pt-4">
+          TrevTutor's Discrete Math 1 playlist{" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-red-300"
+            href="https://www.youtube.com/playlist?list=PLDDGPdw7e6Ag1EIznZ-m-qXu4XX3A0cIz"
+          >
+            Youtube
+          </a>
+        </div>
+        <div className="pt-3">
+          3Blue1Brown's Essence of Linear Algebra playlist{" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-red-300"
+            href="https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab"
+          >
+            Youtube
+          </a>
+        </div>
+        <div className="pt-3">
+          Abdul Bari's Algorithms playlist{" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-red-300"
+            href="https://www.youtube.com/playlist?list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O"
+          >
+            Youtube
+          </a>
+        </div>
+        <div className="pt-3">
+          The Net Ninja (various playlists){" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-red-300"
+            href="https://www.youtube.com/c/TheNetNinja"
+          >
+            Youtube
+          </a>
+        </div>
+        <div className="pt-3">
+          William Fiset (various playlists){" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-red-300"
+            href="https://www.youtube.com/c/WilliamFiset-videos"
+          >
+            Youtube
+          </a>
+        </div>
+        <div className="pt-3">
+          Composing Programs (John DeNero){" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-emerald-200"
+            href="https://composingprograms.com/"
+          >
+            Online Textbook
+          </a>
+        </div>
+        <div className="pt-3">
+          Full Stack Open 2022{" "}
+          <a
+            className="px-1.5 py-0.5 mx-1 rounded-md bg-violet-200"
+            href="https://fullstackopen.com/en/"
+          >
+            Online Course
+          </a>
+        </div>
+      </div>
+      <div className="text-lg pt-4">Happy learning!</div>
+    </div>
+  );
+};
+
+export default Resources;
