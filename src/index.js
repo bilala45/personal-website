c82b62d28ed602c4287090226acb50e3672030dd
Bilal Ali <bilalal495@outlook.com>
2022-08-16 21:01:20 -0400

    Update base url in browser router component

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
