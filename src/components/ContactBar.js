5873b5007b05f1b86527b832daf93975b36b7daa
Bilal Ali <bilalal495@outlook.com>
2023-09-05 12:13:58 -0400

    Fix contact bar shrinking on small screen sizes

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -3,21 +3,21 @@ import { FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa";
 const ContactBar = () => (
   // add padding above and below links and center
   <div className="flex justify-center mt-6">
-    <div className="flex p-1 sm:px-1.5 rounded-full max-w-fit bg-sky-500">
+    <div className="flex p-1 px-1.5 rounded-full max-w-fit bg-sky-500">
       <a
-        className="text-2xl sm:text-3xl px-3 sm:px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
+        className="text-3xl px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
         href="https://github.com/bilala45"
       >
         <FaGithub />
       </a>
       <a
-        className="text-2xl sm:text-3xl px-3 sm:px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
+        className="text-3xl px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
         href="https://www.linkedin.com/in/bilal-ali1/"
       >
         <FaLinkedinIn />
       </a>
       <a
-        className="text-2xl sm:text-3xl px-3 sm:px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
+        className="text-3xl px-4 py-1 text-white rounded-full hover:text-black hover:bg-white"
         href="mailto: bilalali@seas.upenn.edu"
       >
         <FaEnvelope />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     // wrapper inherits parent height (screen height)
     <div className="px-5 pb-20 text-center">
-      <h1 className="text-4xl md:text-5xl font-semibold">Bilal Ali</h1>
-      <h2 className="text-xl md:text-xl mt-5 tracking-wide font-light ">
+      <h1 className="text-5xl font-semibold">Bilal Ali</h1>
+      <h2 className="text-xl mt-5 tracking-wide font-light ">
         computer science @ upenn
       </h2>
       <ContactBar />
