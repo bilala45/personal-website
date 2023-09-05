405dc9163ae344dd030833755fb34e380dacdf41
Bilal Ali <bilalal495@outlook.com>
2023-09-05 00:48:23 -0400

    Update website content

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,49 @@
+import ResourceLink from "./utils/ResourceLink";
+
 const About = () => {
   return (
     <div className="text-md">
       <div className="md:hidden font-semibold text-2xl text-sky-600">About</div>
-      <div className="pt-5 md:pt-0 font-semibold text-lg">
+
+      {/* <div className="pt-5 md:pt-0 font-semibold text-lg">
         Hey, I'm Bilal! &#128075;&#127997;
-      </div>
+      </div> */}
+
+      <div className="pt-5 font-semibold md:pt-0 text-lg">currently</div>
+
       <div className="pt-3">
-        I'm a Master's student studying computer science at the University of
-        Pennsylvania.
+        I'm a Master's student (graduating 2025) at the{" "}
+        <ResourceLink
+          link="https://www.upenn.edu/"
+          name=" University of Pennsylvania"
+        />
+        . I study computer science and I'm TAing an intro software dev class!
       </div>
+
+      <div className="pt-5 font-semibold text-lg">previously</div>
+
       <div className="pt-3">
-        Previously, in 2021, I graduated from McGill University. During my
-        studies, I've interned at{" "}
-        <a
-          className="text-sky-600 hover:underline hover:underline-offset-4 decoration-1 decoration-slate-300"
-          href="https://www.normative.ca/"
-        >
-          Normative
-        </a>
-        , a small biotech startup, and most recently at{" "}
-        <a
-          className="underline hover:text-sky-600 underline-offset-4 decoration-1 decoration-slate-300"
-          href="https://www.genesys.com/"
-        >
-          Genesys
-        </a>
-        , a large cloud-based SaaS company. Currently, I'm TA'ing CIT 5910:
-        Introduction to Software Development.
+        Interned with{" "}
+        <ResourceLink link="https://www.genesys.com/" name="Genesys" />,
+        developing scalable, cloud-based microservices.
       </div>
+
       <div className="pt-3">
-        Things I'm interested in: backend engineering, software system design,
-        and distributed systems.
+        Interned for a small biotech startup called{" "}
+        <ResourceLink link="https://www.normative.ca/" name="Normative" />,
+        developing their primary web app.
+      </div>
+
+      <div className="pt-3">
+        Graduated from{" "}
+        <ResourceLink link="https://www.mcgill.ca/" name=" McGill University" />{" "}
+        in 2021.
+      </div>
+
+      <div className="pt-5 font-semibold text-lg">interests</div>
+
+      <div className="pt-3">
+        backend engineering, software system design, distributed systems
       </div>
     </div>
   );
